bfda942b1026c298de50b319db461e4ed591c03f
Danilo Canivel <dccanive@amazon.com>
2021-01-29 08:54:06 -0500

    add hhs bed capacity cdk

--- lib/juvare-stack.ts
+++ lib/juvare-stack.ts
@@ -1,14 +1,14 @@
-import s3 = require('@aws-cdk/aws-s3');
-import s3n = require('@aws-cdk/aws-s3-notifications');
-import s3deploy = require('@aws-cdk/aws-s3-deployment');
-import kms = require('@aws-cdk/aws-kms');
-import iam = require('@aws-cdk/aws-iam');
-import dynamodb = require('@aws-cdk/aws-dynamodb');
-import glue = require('@aws-cdk/aws-glue');
-import sns = require('@aws-cdk/aws-sns');
-import lambda = require('@aws-cdk/aws-lambda');
-import {App, CfnOutput, Duration, RemovalPolicy, Stack, StackProps} from "@aws-cdk/core";
-import {createLambdaWithLayer, creates3bucket} from "./helpers";
+import s3 = require("@aws-cdk/aws-s3");
+import s3n = require("@aws-cdk/aws-s3-notifications");
+import s3deploy = require("@aws-cdk/aws-s3-deployment");
+import kms = require("@aws-cdk/aws-kms");
+import iam = require("@aws-cdk/aws-iam");
+import dynamodb = require("@aws-cdk/aws-dynamodb");
+import glue = require("@aws-cdk/aws-glue");
+import sns = require("@aws-cdk/aws-sns");
+import lambda = require("@aws-cdk/aws-lambda");
+import { App, CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from "@aws-cdk/core";
+import { createLambda, createLambdaWithLayer, creates3bucket } from "./helpers";
 
 export interface juvareStackProps extends StackProps {
   readonly envName: string;
@@ -18,62 +18,67 @@ export class juvareStack extends Stack {
   constructor(app: App, id: string, props: juvareStackProps) {
     super(app, id, props);
 
-    const envName = props.envName
+    const envName = props.envName;
 
     // IAM role for processCCDA, .. add more tbd
 
-    const roleLambdaProcessJuvare = new iam.Role(this, 'roleLambdaProcessJuvare',{
+    const roleLambdaProcessJuvare = new iam.Role(this, "roleLambdaProcessJuvare", {
       assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-      roleName: envName+'LambdaProcessJuvare'
+      roleName: envName + "LambdaProcessJuvare",
     });
-    roleLambdaProcessJuvare.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole'))
-    roleLambdaProcessJuvare.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaRole'))
-    roleLambdaProcessJuvare.addToPolicy(new iam.PolicyStatement(
-      {
-        resources: ['*'],
-        actions: ['glue:StartCrawler']
-      }))
+    roleLambdaProcessJuvare.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaVPCAccessExecutionRole")
+    );
+    roleLambdaProcessJuvare.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaRole")
+    );
+    roleLambdaProcessJuvare.addToPolicy(
+      new iam.PolicyStatement({
+        resources: ["*"],
+        actions: ["glue:StartCrawler"],
+      })
+    );
 
     // glue service role
     //const roleGlueService = iam.Role.fromRoleArn(this, 'roleGlueService','arn:aws:iam::'+this.account+':role/AWSGlueServiceRole-'+envName)
-    const roleGlueService = new iam.Role(this, 'roleGlueService',{
+    const roleGlueService = new iam.Role(this, "roleGlueService", {
       assumedBy: new iam.CompositePrincipal(
         new iam.ServicePrincipal("glue.amazonaws.com"),
         new iam.ServicePrincipal("lambda.amazonaws.com")
       ),
       // Glue role name must follow the below syntax. AWSGlueServiceRole Prefix is required for Glue to work properly.
-      roleName: "AWSGlueServiceRole-"+envName+'Juvare'
-    })
-    roleGlueService.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'))
-
+      roleName: "AWSGlueServiceRole-" + envName + "Juvare",
+    });
+    roleGlueService.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole")
+    );
 
     // KMS
 
-    const kmsJuvareKey = new kms.Key(this, 'kmsJuvareKey',{
-      alias: envName+'-juvare-key',
-      enableKeyRotation: true
-    })
-    kmsJuvareKey.grantEncryptDecrypt(roleLambdaProcessJuvare)
+    const kmsJuvareKey = new kms.Key(this, "kmsJuvareKey", {
+      alias: envName + "-juvare-key",
+      enableKeyRotation: true,
+    });
+    kmsJuvareKey.grantEncryptDecrypt(roleLambdaProcessJuvare);
 
-    // S3 Buckets
+    // S3 Buckets - Juvare
 
-
-    const s3LandingJuvare = new creates3bucket(this, 'landing-juvare', kmsJuvareKey)
-    s3LandingJuvare.bucket.grantReadWrite(roleLambdaProcessJuvare)
+    const s3LandingJuvare = new creates3bucket(this, "landing-juvare", kmsJuvareKey);
+    s3LandingJuvare.bucket.grantReadWrite(roleLambdaProcessJuvare);
 
     // Create folder structure in Juvare Landing bucket
-    new s3deploy.BucketDeployment(this, 'cdph_idph',{
-      sources: [s3deploy.Source.asset('./juvare_folder_structure/juvare')],
+    new s3deploy.BucketDeployment(this, "cdph_idph", {
+      sources: [s3deploy.Source.asset("./juvare_folder_structure/juvare")],
       destinationBucket: s3LandingJuvare.bucket,
       //destinationKeyPrefix: '',
-    })
+    });
 
-    const s3ProcessedJuvare = new creates3bucket(this, 'processed-juvare', kmsJuvareKey)
-    s3ProcessedJuvare.bucket.grantReadWrite(roleLambdaProcessJuvare)
-    s3ProcessedJuvare.bucket.grantReadWrite(roleGlueService)
+    const s3ProcessedJuvare = new creates3bucket(this, "processed-juvare", kmsJuvareKey);
+    s3ProcessedJuvare.bucket.grantReadWrite(roleLambdaProcessJuvare);
+    s3ProcessedJuvare.bucket.grantReadWrite(roleGlueService);
 
     // Athena Queries bucket. AES256 encrypted to allow users to use the bucket for athena queries
-    const s3AthenaQueries = new s3.Bucket(this, 'athena-queries',{
+    const s3AthenaQueries = new s3.Bucket(this, "athena-queries", {
       encryption: s3.BucketEncryption.S3_MANAGED,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: RemovalPolicy.RETAIN,
@@ -83,133 +88,241 @@ export class juvareStack extends Stack {
           transitions: [
             {
               storageClass: s3.StorageClass.GLACIER,
-              transitionAfter: Duration.days(30)
-            }
-          ]
-        }
-      ]
+              transitionAfter: Duration.days(30),
+            },
+          ],
+        },
+      ],
     });
-    s3AthenaQueries.grantReadWrite(roleGlueService)
+    s3AthenaQueries.grantReadWrite(roleGlueService);
 
+    // S3 Buckets - HHS
 
-    // dynamodb
-    const juvare_hash_table_log = new dynamodb.Table(this, 'juvare_hash_table_log', {
-      tableName: envName+'-juvare_hash_table_log',
+    const s3LandingHHS = new creates3bucket(this, "landing-hhs", kmsJuvareKey);
+    s3LandingHHS.bucket.grantReadWrite(roleLambdaProcessJuvare);
+
+    // Create folder structure in HHS Landing bucket
+    new s3deploy.BucketDeployment(this, "hhs", {
+      sources: [s3deploy.Source.asset("./hhs_folder_structure")],
+      destinationBucket: s3LandingHHS.bucket,
+      //destinationKeyPrefix: '',
+    });
+
+    const s3ProcessedHHS = new creates3bucket(this, "processed-hhs", kmsJuvareKey);
+    s3ProcessedJuvare.bucket.grantReadWrite(roleLambdaProcessJuvare);
+    s3ProcessedJuvare.bucket.grantReadWrite(roleGlueService);
+
+    // dynamodb - juvare
+    const juvare_hash_table_log = new dynamodb.Table(this, "juvare_hash_table_log", {
+      tableName: envName + "-juvare_hash_table_log",
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
       encryptionKey: kmsJuvareKey,
-      partitionKey: {name: 'md5Digest', type: dynamodb.AttributeType.STRING},
+      partitionKey: { name: "md5Digest", type: dynamodb.AttributeType.STRING },
       removalPolicy: RemovalPolicy.DESTROY,
-    })
-    juvare_hash_table_log.grantFullAccess(roleLambdaProcessJuvare)
+    });
+    juvare_hash_table_log.grantFullAccess(roleLambdaProcessJuvare);
 
-    const juvare_execution_log = new dynamodb.Table(this, 'juvare_execution_log', {
-      tableName: envName+'-juvare_execution_log',
+    const juvare_execution_log = new dynamodb.Table(this, "juvare_execution_log", {
+      tableName: envName + "-juvare_execution_log",
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
       encryptionKey: kmsJuvareKey,
-      partitionKey: {name: 'lambdaId', type: dynamodb.AttributeType.STRING},
+      partitionKey: { name: "lambdaId", type: dynamodb.AttributeType.STRING },
       removalPolicy: RemovalPolicy.DESTROY,
-    })
-    juvare_execution_log.grantFullAccess(roleLambdaProcessJuvare)
+    });
+    juvare_execution_log.grantFullAccess(roleLambdaProcessJuvare);
 
+    // dynamodb - hhs
+
+    const hhs_hash_table_log = new dynamodb.Table(this, "hhs_hash_table_log", {
+      tableName: envName + "-hhs_hash_table_log",
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
+      encryptionKey: kmsJuvareKey,
+      partitionKey: { name: "md5Digest", type: dynamodb.AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    hhs_hash_table_log.grantFullAccess(roleLambdaProcessJuvare);
+
+    const hhs_execution_log = new dynamodb.Table(this, "hhs_execution_log", {
+      tableName: envName + "-hhs_execution_log",
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      encryption: dynamodb.TableEncryption.CUSTOMER_MANAGED,
+      encryptionKey: kmsJuvareKey,
+      partitionKey: { name: "lambdaId", type: dynamodb.AttributeType.STRING },
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    hhs_execution_log.grantFullAccess(roleLambdaProcessJuvare);
 
     //
-    // SNS
+    // SNS - Juvare
     //
 
-    const JuvareProcessingTopic = new sns.Topic(this, 'snsJuvareProcessingTopic', {
-      displayName: envName+'JuvareProcessingTopic'
-    })
+    const JuvareProcessingTopic = new sns.Topic(this, "snsJuvareProcessingTopic", {
+      displayName: envName + "JuvareProcessingTopic",
+    });
 
     //
-    // Glue
+    // SNS - HHS
     //
 
-    const glueDbJuvare = new glue.Database(this, envName+'juvare', {
-      databaseName: envName+'-juvare'
-    })
+    const HHSProcessingTopic = new sns.Topic(this, "snsHHSProcessingTopic", {
+      displayName: envName + "HHSProcessingTopic",
+    });
 
+    //
+    // Glue - Juvare
+    //
 
+    const glueDbJuvare = new glue.Database(this, envName + "juvare", {
+      databaseName: envName + "-juvare",
+    });
 
-    const JuvareDailyCDPHIDPHCrawler = new glue.CfnCrawler(this, 'JuvareDailyCDPHIDPHCrawler',{
-      name: envName+'JuvareDailyCDPHIDPHCrawler',
+    const JuvareDailyCDPHIDPHCrawler = new glue.CfnCrawler(this, "JuvareDailyCDPHIDPHCrawler", {
+      name: envName + "JuvareDailyCDPHIDPHCrawler",
       role: roleGlueService.roleArn,
       databaseName: glueDbJuvare.databaseName,
-      schedule: {"scheduleExpression": "cron(0 0 * * ? *)"},
-      tablePrefix: 'daily_cpdh_idph_',
+      schedule: { scheduleExpression: "cron(0 0 * * ? *)" },
+      tablePrefix: "daily_cpdh_idph_",
       targets: {
-        s3Targets: [{
-          path: 's3://'+s3ProcessedJuvare.bucket.bucketName+'/juvare/cdph_idph/'
-        }]
-        }
-      })
+        s3Targets: [
+          {
+            path: "s3://" + s3ProcessedJuvare.bucket.bucketName + "/juvare/cdph_idph/",
+          },
+        ],
+      },
+    });
 
-    const JuvareDailyHaveBedCrawler = new glue.CfnCrawler(this, 'JuvareDailyHaveBedCrawler',{
-      name: envName+'JuvareDailyHaveBedCrawler',
+    const JuvareDailyHaveBedCrawler = new glue.CfnCrawler(this, "JuvareDailyHaveBedCrawler", {
+      name: envName + "JuvareDailyHaveBedCrawler",
       role: roleGlueService.roleArn,
       databaseName: glueDbJuvare.databaseName,
-      tablePrefix: 'daily_have_bed_',
-      schedule: {"scheduleExpression": "cron(0 0 * * ? *)"},
+      tablePrefix: "daily_have_bed_",
+      schedule: { scheduleExpression: "cron(0 0 * * ? *)" },
       targets: {
-        s3Targets: [{
-          path: 's3://'+s3ProcessedJuvare.bucket.bucketName+'/juvare/daily_havbed/'
-        }]
-      }
-    })
+        s3Targets: [
+          {
+            path: "s3://" + s3ProcessedJuvare.bucket.bucketName + "/juvare/daily_havbed/",
+          },
+        ],
+      },
+    });
+
+    //
+    // Glue - HHS
+    //
+
+    const glueDbHHS = new glue.Database(this, envName + "hhs", {
+      databaseName: envName + "-hhs",
+    });
+
+    const HHSDailyCDPHIDPHCrawler = new glue.CfnCrawler(this, "HHSCrawler", {
+      name: envName + "HHSCrawler",
+      role: roleGlueService.roleArn,
+      databaseName: glueDbJuvare.databaseName,
+      schedule: { scheduleExpression: "cron(0 0 * * ? *)" },
+      tablePrefix: "hhs_",
+      targets: {
+        s3Targets: [
+          {
+            path: "s3://" + s3ProcessedHHS.bucket.bucketName,
+          },
+        ],
+      },
+    });
 
     // lambda layers
-    const layerXlrd = new lambda.LayerVersion(this, 'xlrd', {
-      code: lambda.Code.fromAsset('lambda_layer/xlrd.zip'),
+    const layerXlrd = new lambda.LayerVersion(this, "xlrd", {
+      code: lambda.Code.fromAsset("lambda_layer/xlrd.zip"),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_8],
-      layerVersionName: envName+'-xlrd'
-    })
+      layerVersionName: envName + "-xlrd",
+    });
 
-    // lambda
+    // lambda - Juvare
 
-    const juvare_cdph_idph_process = new createLambdaWithLayer(this, envName, roleLambdaProcessJuvare, 'juvare_cdph_idph_process',layerXlrd,
+    const juvare_cdph_idph_process = new createLambdaWithLayer(
+      this,
+      envName,
+      roleLambdaProcessJuvare,
+      "juvare_cdph_idph_process",
+      layerXlrd,
       {
         BUCKET_PROCESSED_JUVARE: s3ProcessedJuvare.bucket.bucketName,
-        BUCKET_PROCESSED_JUVARE_FOLDER: 'cdph_idph',
-        BUCKET_RAW_JUVARE_FOLDER: 'raw_cdph_idph',
+        BUCKET_PROCESSED_JUVARE_FOLDER: "cdph_idph",
+        BUCKET_RAW_JUVARE_FOLDER: "raw_cdph_idph",
         DYNAMODB_JUVARE_EXECUTION_LOG: juvare_execution_log.tableName,
         DYNAMODB_JUVARE_HASH_TABLE_LOG: juvare_hash_table_log.tableName,
-        GLUE_CRAWLER_JUVARE_CDPH_IDPH: envName+'JuvareDailyCDPHIDPHCrawler',
-        SNS_TOPIC_ARN: JuvareProcessingTopic.topicArn
-      });
+        GLUE_CRAWLER_JUVARE_CDPH_IDPH: envName + "JuvareDailyCDPHIDPHCrawler",
+        SNS_TOPIC_ARN: JuvareProcessingTopic.topicArn,
+      }
+    );
 
     // todo add crawler, glue db
 
-    const juvare_have_bed_process = new createLambdaWithLayer(this, envName, roleLambdaProcessJuvare, 'juvare_have_bed_process',layerXlrd,
+    const juvare_have_bed_process = new createLambdaWithLayer(
+      this,
+      envName,
+      roleLambdaProcessJuvare,
+      "juvare_have_bed_process",
+      layerXlrd,
       {
         BUCKET_PROCESSED_JUVARE: s3ProcessedJuvare.bucket.bucketName,
-        BUCKET_PROCESSED_JUVARE_FOLDER: 'daily_havbed',
-        BUCKET_RAW_JUVARE_FOLDER: 'raw_daily_havbed',
+        BUCKET_PROCESSED_JUVARE_FOLDER: "daily_havbed",
+        BUCKET_RAW_JUVARE_FOLDER: "raw_daily_havbed",
         DYNAMODB_JUVARE_EXECUTION_LOG: juvare_execution_log.tableName,
         DYNAMODB_JUVARE_HASH_TABLE_LOG: juvare_hash_table_log.tableName,
-        GLUE_CRAWLER_JUVARE_HAVE_BED: envName+'JuvareDailyHaveBedCrawler',
-        SNS_TOPIC_ARN: JuvareProcessingTopic.topicArn
-      });
+        GLUE_CRAWLER_JUVARE_HAVE_BED: envName + "JuvareDailyHaveBedCrawler",
+        SNS_TOPIC_ARN: JuvareProcessingTopic.topicArn,
+      }
+    );
 
-    // S3 Triggers/Notifications
+    // lambda - HHS
 
-    s3LandingJuvare.bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+    const hhs_bed_capacity = new createLambda(
+      this,
+      envName,
+      roleLambdaProcessJuvare,
+      "hhs_bed_capacity",
+      {
+        BUCKET_PROCESSED_HHS: s3ProcessedHHS.bucket.bucketName,
+        DYNAMODB_HHS_EXECUTION_LOG: hhs_execution_log.tableName,
+        DYNAMODB_HHS_HASH_TABLE_LOG: hhs_hash_table_log.tableName,
+        GLUE_CRAWLER_HHS: envName + "HHSCrawler",
+        SNS_TOPIC_ARN: HHSProcessingTopic.topicArn,
+      }
+    );
+
+    // S3 Triggers/Notifications - Juvare
+
+    s3LandingJuvare.bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
       new s3n.LambdaDestination(juvare_cdph_idph_process.lambdaFunction),
       {
-        prefix: 'cdph_idph/'
-    })
+        prefix: "cdph_idph/",
+      }
+    );
 
-    s3LandingJuvare.bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
+    s3LandingJuvare.bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
       new s3n.LambdaDestination(juvare_have_bed_process.lambdaFunction),
       {
-        prefix: 'daily_havbed/'
-      })
+        prefix: "daily_havbed/",
+      }
+    );
 
-    new CfnOutput(this, 'roleGlueServiceExport', {
+    // S3 Triggers/Notifications - HHS
+
+    s3LandingHHS.bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(hhs_bed_capacity.lambdaFunction)
+    );
+
+    // CFN Outputs
+
+    new CfnOutput(this, "roleGlueServiceExport", {
       value: roleGlueService.roleArn,
-      exportName: envName+'-roleGlueService'
+      exportName: envName + "-roleGlueService",
     });
-
-
   }
 }
